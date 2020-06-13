a619376ed395f593522a275f550509dd26cb31fc
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-06-13 13:02:17 +0200

    Initial Commit

---
+++ SwiftUIMenuBar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIMenuBar
+//
+//  Created by Felicitas Pojtinger on 13.06.20.
+//  Copyright © 2020 Felicitas Pojtinger. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
