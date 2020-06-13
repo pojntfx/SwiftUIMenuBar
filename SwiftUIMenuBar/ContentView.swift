d9b7009c7b5be2de4dbb9245fecf3cbb36e93cc2
Felix Pojtinger <felix@pojtinger.com>
2020-06-13 15:08:56 +0200

    feat: Add basic todo app form

--- SwiftUIMenuBar/ContentView.swift
+++ SwiftUIMenuBar/ContentView.swift
@@ -8,15 +8,48 @@
 
 import SwiftUI
 
+struct TodoModel: Identifiable, Codable {
+    var id = UUID()
+    var title: String
+}
+
+class TodoViewModel: ObservableObject {
+    @Published var todos: [TodoModel] = []
+    
+    func createTodo(todo: TodoModel) {
+        todos.append(todo)
+    }
+}
+
 struct ContentView: View {
+    @ObservedObject var todoVM = TodoViewModel()
+    @State var newTodo = ""
+    
     var body: some View {
-        VStack {
-            Button("Add Todo") {
-                print("Clicked")
+        Form {
+            List(todoVM.todos) { todo in
+                Text(todo.title)
+            }
+            HStack {
+                TextField("New todo", text: $newTodo, onCommit: createTodo)
+                Button(action: createTodo) {
+                    HStack {
+                        Image(nsImage: NSImage(named: NSImage.addTemplateName)!)
+                        Text("Create")
+                    }
+                }
             }
             
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding()
         }
+    }
+    
+    func createTodo() {
+        if !self.newTodo.isEmpty {
+           self.todoVM.createTodo(todo: TodoModel(title: self.newTodo))
+           
+           self.newTodo = ""
+       }
     }
 }
 
