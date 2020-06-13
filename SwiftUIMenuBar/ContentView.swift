63628cdc18f29def776f2b47a8bece795ab4b13f
Felix Pojtinger <felix@pojtinger.com>
2020-06-13 13:02:17 +0200

    Initial Commit

---
+++ SwiftUIMenuBar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIMenuBar
+//
+//  Created by Felix Pojtinger on 13.06.20.
+//  Copyright © 2020 Felix Pojtinger. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
